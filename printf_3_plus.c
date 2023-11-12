55bfac8af690dc932ffdde5442a6208f8f6582c1
AsitaEmmanuella <asitemmanuella19@gmail.com>
2023-11-12 14:11:34 -0800

    print

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdarg.h>
 
 /**
@@ -9,24 +8,32 @@
 
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
+  va_list args;
+  int count = 0;
 
-	va_start(args, format);
-	while (*format != '\0')
-	{
-		if (*format == '%')
-		{
-			/* Handle special format specifiers */
-			count += print_special_format(format, args);
-			format += 2;
-		} else
-		{
-			/* Print the character */
-			count += putchar(*format);
-			format++;
-		}
-	}
-va_end(args);
-return (count);
+  va_start(args, format);
+  while (*format != '\0') {
+    if (*format == '%') {
+      switch (*++format) {
+        case 'c':
+          count += putchar(va_arg(args, int));
+          break;
+        case 's':
+          count += fputs(va_arg(args, const char*), stdout);
+          break;
+        case '%':
+          count += putchar('%');
+          break;
+        default:
+          /* Ignore unsupported conversion specifiers. */
+          break;
+      }
+    } else {
+      count += putchar(*format);
+    }
+    format++;
+  }
+  va_end(args);
+
+  return count;
 }
