f678909ef4354a720e9051f7c04f810a1212e5b5
AsitaEmmanuella <asitemmanuella19@gmail.com>
2023-11-11 16:44:42 -0800

    print_3

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+/**
+ * _printf - prints to the standard output
+ * @format: format string
+ * Return: number of bytes printed
+ */
+
 int _printf(const char *format, ...) {
-  va_list args;
-  int count = 0;
+	va_list args;
+	int count = 0;
 
-  va_start(args, format);
-  while (*format != '\0') {
-    if (*format == '%') {
-      
-      count += print_special_format(format, args);
-      format += 2;
-    } else {
-      
-      count += putchar(*format);
-      format++;
-    }
-  }
-  va_end(args);
+	va_start(args, format);
+	while (*format != '\0') {
+		if (*format == '%') {
+			/* Handle special format specifiers */
 
-  return count;
+			count += print_special_format(format, args);
+			format += 2;
+		} else {
+			/* Print the character */
+			count += putchar(*format);
+			format++;
+		}
+	}
+	va_end(args);
+
+	return (count);
 }
