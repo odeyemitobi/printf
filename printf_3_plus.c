9d4831437676b6e4e9255ac4f52c5893a3f1b199
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-12 14:37:04 -0800

    printf_3_plus.c

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -8,32 +8,35 @@
 
 int _printf(const char *format, ...)
 {
-  va_list args;
-  int count = 0;
+	va_list args;
+	int count = 0;
 
-  va_start(args, format);
-  while (*format != '\0') {
-    if (*format == '%') {
-      switch (*++format) {
-        case 'c':
-          count += putchar(va_arg(args, int));
-          break;
-        case 's':
-          count += fputs(va_arg(args, const char*), stdout);
-          break;
-        case '%':
-          count += putchar('%');
-          break;
-        default:
-          /* Ignore unsupported conversion specifiers. */
-          break;
-      }
-    } else {
-      count += putchar(*format);
-    }
-    format++;
-  }
-  va_end(args);
-
-  return count;
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			switch (*++format)
+			{
+				case 'c':
+					count += putchar(va_arg(args, int));
+					break;
+				case 's':
+					count += fputs(va_arg(args, const char*), stdout);
+					break;
+				case '%':
+					count += putchar('%');
+					break;
+				default:
+					/* Ignore unsupported conversion specifiers. */
+					break;
+			}
+		} else
+		{
+			count += putchar(*format);
+		}
+		format++;
+	}
+va_end(args);
+return (count);
 }
