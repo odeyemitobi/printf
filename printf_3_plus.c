cee1159acbe9b6603b55a281708288ec0f0674ea
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-12 13:28:40 -0800

    printf

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -7,24 +7,26 @@
  * Return: number of bytes printed
  */
 
-int _printf(const char *format, ...) {
+int _printf(const char *format, ...)
+{
 	va_list args;
 	int count = 0;
 
 	va_start(args, format);
-	while (*format != '\0') {
-		if (*format == '%') {
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
 			/* Handle special format specifiers */
-
 			count += print_special_format(format, args);
 			format += 2;
-		} else {
+		} else
+		{
 			/* Print the character */
 			count += putchar(*format);
 			format++;
 		}
 	}
-	va_end(args);
-
+	va_end(args);	
 	return (count);
 }
