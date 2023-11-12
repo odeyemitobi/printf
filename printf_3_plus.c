92a072328b806ad8f9c3d2aa6008e74e03d933e8
AsitaEmmanuella <asitemmanuella19@gmail.com>
2023-11-12 14:43:48 -0800

    print

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -1,4 +1,4 @@
-#include <stdarg.h>
+#include "main.h"
 
 /**
  * _printf - prints to the standard output
@@ -7,36 +7,46 @@
  */
 
 int _printf(const char *format, ...)
-{
-	va_list args;
-	int count = 0;
 
-	va_start(args, format);
-	while (*format != '\0')
+{
+	int add = 0;
+	va_list xy;
+	char *p, *start;
+
+	params_t params = PARAMS_INIT;
+
+	va_start(xy, format);
+
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
 	{
-		if (*format == '%')
+		init_params(&params, xy);
+		if (*p != '%')
 		{
-			switch (*++format)
-			{
-				case 'c':
-					count += putchar(va_arg(args, int));
-					break;
-				case 's':
-					count += fputs(va_arg(args, const char*), stdout);
-					break;
-				case '%':
-					count += putchar('%');
-					break;
-				default:
-					/* Ignore unsupported conversion specifiers. */
-					break;
-			}
-		} else
-		{
-			count += putchar(*format);
+			add += _putchar(*p);
+			continue;
 		}
-		format++;
+		start = p;
+		p++;
+		while (get_flag(p, &params))
+		{
+			p++;
+		}
+		p = get_width(p, &params, xy);
+		p = get_precision(p, &params, xy);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			add += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			add += get_print_func(p, xy, &params);
 	}
-va_end(args);
-return (count);
+	_putchar(BUF_FLUSH);
+	va_end(xy);
+	return (add);
 }
+
