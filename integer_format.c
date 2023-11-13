c5bfffaf895417e9ab227adf6221b9e778c626e6
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-13 06:43:06 -0800

    integer_format.c

--- integer_format.c
+++ integer_format.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ * get_precision - precision string
+ * @s: string
+ * @params: the parameters struct
+ * @xy: pointer
+ * Return: new pointer
+*/
+
 char *get_precision(char *s, params_t *params, va_list xy)
 {
 	if (*s != '.')
