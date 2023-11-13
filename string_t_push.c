1b82c8064bd786fa548b8247c3af9ff700d0d9b5
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-13 09:48:12 -0800

    string_t_push.c

--- string_t_push.c
+++ string_t_push.c
@@ -1,50 +1,35 @@
-STRING_T_PUSH.C
-
 #include "main.h"
-
-
 
 int print_from_to(char *start, char *stop, char *except)
 {
 	int add = 0;
-
-	while (start <= stop)
-	{
-		if (start != except)
-			add += _putchar(*start);
-		start++;
-	}
+	while (start <= stop && 
+			(add += (start != except) ? _putchar(*start) : 0, start++, 1));
 	return (add);
 }
 
 
-
-int print_rev(va_list xy, params_t *params) {
+int print_rev(va_list xy, params_t *params)
+{
+	int add = 0;
 	char *str = va_arg(xy, char *);
 	(void)params;
-
-	print_custom(str, [](char c) -> int { return _putchar(c); });
-
-	return (0);
-}
-
-int print_rot13(va_list xy, params_t *params) {
-	char *str = va_arg(xy, char *);
-	(void)params;
-
-	print_custom(str, [](char c) -> int {
-			int k = c - 65;
-			char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLM   nopqrstuvwxyzabcdefghijklm";
-
-			if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-			return _putchar(arr[k]);
-			else
-			return _putchar(c);
-			});
-
-	return (0);
+	if (str)
+		for (int len = 0; *str; str++, len++)
+			add += _putchar(str[-1]);
+	return (add);
 }
 
 
+int print_rot13(va_list xy, params_t *params)
+{
+	int c = 0, k = 0, r = 0;
+	char arr[] = "NOPQRSTUVWXYZABCDEFGHIJKLM      nopqrstuvwxyzabcdefghijklm";
+	char *a = va_arg(xy, char *);
+	(void)params;
 
-
+	while (a[r] && (c += (a[r] >= 'A' && a[r] <= 'Z') || (a[r] >= 'a' && a[r] <= 'z')
+				? _putchar(arr[a[r] - 'A'])
+				: _putchar(a[r]), r++, 1));
+	return (c);
+}
