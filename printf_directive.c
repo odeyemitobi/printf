81bf1c426012a24b8a9b9c1e29b2da2fb329ebf3
AsitaEmmanuella <asitemmanuella19@gmail.com>
2023-11-13 05:51:13 -0800

    dir

--- printf_directive.c
+++ printf_directive.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * init_params - clears struct
+ * @params:  parameters struct
+ * @xy:  argument pointer
+ * Return: none
+ */
+
 void init_params(params_t *params, va_list xy)
 {
 	params->flags = 0;
