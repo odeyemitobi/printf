9a8d6d14c4d00e80c91ef99dec3bcbd5f7884611
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-13 22:43:30 -0800

    fixing error

--- deci_binary.c
+++ deci_binary.c
@@ -6,10 +6,27 @@
  * @params: struct
  * Return: printed
  */
-
 int print_hex(va_list xy, params_t *params)
 {
-	return (print_number_base(xy, params, 16));
+	unsigned long l;
+	int q = 0;
+	char *str;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(xy, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(xy, unsigned int);
+	else
+		l = (unsigned int)va_arg(xy, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED | CONVERT_LOWERCASE, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'x';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (q += print_number(str, params));
 }
 
 /**
@@ -18,22 +35,44 @@ int print_hex(va_list xy, params_t *params)
  * @params: struct
  * Return: printed
  */
-
 int print_HEX(va_list xy, params_t *params)
 {
-	return (print_number_base(xy, params, 16));
-}
+	unsigned long l;
+	int q = 0;
+	char *str;
 
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(xy, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(xy, unsigned int);
+	else
+		l = (unsigned int)va_arg(xy, unsigned int);
+
+	str = convert(l, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (q += print_number(str, params));
+}
 /**
  * print_binary - number
  * @xy: pointer
  * @params: struct
  * Return: bytes printed
  */
-
 int print_binary(va_list xy, params_t *params)
 {
-	return (print_number_base(xy, params, 2));
+	unsigned int n = va_arg(xy, unsigned int);
+	char *str = convert(n, 2, CONVERT_UNSIGNED, params);
+	int q = 0;
+
+	if (params->hashtag_flag && n)
+		*--str = '0';
+	params->unsign = 1;
+	return (q += print_number(str, params));
 }
 
 /**
@@ -43,8 +82,22 @@ int print_binary(va_list xy, params_t *params)
  *
  * Return: printed
  */
-
 int print_octal(va_list xy, params_t *params)
 {
-	return (print_number_base(xy, params, 8));
+	unsigned long l;
+	char *str;
+	int q = 0;
+
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(xy, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(xy, unsigned int);
+	else
+		l = (unsigned int)va_arg(xy, unsigned int);
+	str = convert(l, 8, CONVERT_UNSIGNED, params);
+
+	if (params->hashtag_flag && l)
+		*--str = '0';
+	params->unsign = 1;
+	return (q += print_number(str, params));
 }
