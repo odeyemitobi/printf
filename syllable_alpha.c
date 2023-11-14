00e1163f3897f651cbb70a82f184797284c4bc17
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-14 03:46:28 -0800

    Another fixed

--- syllable_alpha.c
+++ syllable_alpha.c
@@ -14,10 +14,9 @@ int print_char(va_list xy, params_t *params)
 	int pad = params->width - 1;
 	int add = (params->minus_flag) ? _putchar(ch) + pad : pad + _putchar(ch)
 		;
-	
-	while (pad-- > 0) add += _putchar(' ')
+	while (pad-- > 0)
+		add += _putchar(' ')
 		;
-	
 	return (add);
 }
 
@@ -36,14 +35,38 @@ int print_int(va_list xy, params_t *params)
 	return (print_number(convert(l, 10, 0, params), params));
 }
 
+/**
+ * print_string - prints string
+ * @xy:  pointer
+ * @params: parameters struct
+ *
+ * Return: number of characters printed
+ */
+
 int print_string(va_list xy, params_t *params)
 {
-	char *str = va_arg(xy, char *);
-	if (!str) str = NULL_STRING;
-	unsigned int pad = params->width - _strnlen(str, params->precision);
-	int add = (params->minus_flag) ? _puts(str, params->precision) + pad : pad + _puts(str, params->precision);
+	char *str = va_arg
+		(xy, char *)
+		;
+	if (!str)
+		str = NULL_STRING
+			;
+	unsigned int pad = params->width - _strnlen
+		(str, params->precision)
+		;
+	int add = (params->minus_flag) ? _puts(str, params->precision) +
+		pad : pad + _puts(str, params->precision)
+		;
 	return (add);
 }
+
+/**
+ * print_percent - prints
+ * @xy: pointer
+ * @params: struct
+ *
+ * Return: number of characters printed
+ */
 
 int print_percent(va_list xy, params_t *params)
 {
@@ -52,17 +75,28 @@ int print_percent(va_list xy, params_t *params)
 	return (_putchar('%'));
 }
 
+/**
+ * print_S - format specifier
+ * @xy: pointer
+ * @params: parameters
+ *
+ * Return: number of characters printed
+ */
+
 int print_S(va_list xy, params_t *params)
 {
-	char *str = va_arg(xy, char *);
-	if (!str) return _puts(NULL_STRING);
-	int add = 0;
+	char *str = va_arg(xy, char *)
+		;
+	if (!str)
+		return _puts(NULL_STRING)
+		;
+	int add = 0
+		;
+
 	for (; *str; str++)
-		add += (*str > 0 && *str < 32) || *str >= 127 ? _putchar('\\') + _putchar('x') + ((_puts(convert(*str, 16, 0, params) + 1)) ? 1 : _putchar('0')) + _puts(convert(*str, 16, 0, params)) : _putchar(*str);
+		add += (*str > 0 && *str < 32) || *str >= 127 ? _putchar('\\') +
+			_putchar('x') + ((_puts(convert(*str, 16, 0, params) + 1)) ? 1 :
+			 _putchar('0')) + _puts(convert(*str, 16, 0, params)) : _putchar(*str)
+			;
 	return (add);
 }
-
-
-
-
-
