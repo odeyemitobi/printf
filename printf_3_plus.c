bfee50eda1af82283688bbebdef67edfd674ea34
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-14 01:27:56 -0800

    Another fix

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -6,7 +6,6 @@
  * Return: number of bytes printed
  */
 
-
 int _printf(const char *format, ...)
 {
 	int add = 0;
@@ -16,8 +15,9 @@ int _printf(const char *format, ...)
 
 	va_start(xy, format);
 
-	if (!format || (format[0] == '%' &&
-				(!format[1] || (format[1] == ' ' && !format[2]))))
+	if (!format || (format[0] == '%' && !format[1]))
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
 		return (-1);
 
 	for (p = (char *)format; *p; p++)
@@ -39,10 +39,9 @@ int _printf(const char *format, ...)
 
 		if (get_modifier(p, &params))
 			p++;
-
 		if (!get_specifier(p))
-			add += print_from_to(start, p,
-					params.l_modifier || params.h_modifier ? p - 1 : 0);
+			add += print_from_to(start, p, params.l_modifier ||
+					params.h_modifier ? p - 1 : 0);
 		else
 			add += get_print_func(p, xy, &params);
 	}
