fe1bddf80b792219b7aa04e70479a338adc5d175
odeyemitobi <odeyemioluwatobiloba11@gmail.com>
2023-11-14 03:13:34 -0800

    y

--- syllable_alpha.c
+++ syllable_alpha.c
@@ -4,9 +4,12 @@ int print_char(va_list xy, params_t *params)
 {
 	int ch = va_arg(xy, int);
 	int pad = params->width - 1;
-	int add = (params->minus_flag) ? _putchar(ch) + pad : pad + _putchar(ch);
+	int add = (params->minus_flag) ? _putchar(ch) + pad : pad + _putchar(ch)
+		;
+	
 	while (pad-- > 0) add += _putchar(' ')
 		;
+	
 	return (add);
 }
 
@@ -14,7 +17,7 @@ int print_int(va_list xy, params_t *params)
 {
 	long l = (params->l_modifier) ? va_arg(xy, long) :
 		 (params->h_modifier) ? (short int)va_arg(xy, int) : va_arg(xy, int);
-	return print_number(convert(l, 10, 0, params), params);
+	return (print_number(convert(l, 10, 0, params), params));
 }
 
 int print_string(va_list xy, params_t *params)
@@ -23,14 +26,14 @@ int print_string(va_list xy, params_t *params)
 	if (!str) str = NULL_STRING;
 	unsigned int pad = params->width - _strnlen(str, params->precision);
 	int add = (params->minus_flag) ? _puts(str, params->precision) + pad : pad + _puts(str, params->precision);
-	return add;
+	return (add);
 }
 
 int print_percent(va_list xy, params_t *params)
 {
 	(void)xy;
 	(void)params;
-	return _putchar('%');
+	return (_putchar('%'));
 }
 
 int print_S(va_list xy, params_t *params)
@@ -40,7 +43,7 @@ int print_S(va_list xy, params_t *params)
 	int add = 0;
 	for (; *str; str++)
 		add += (*str > 0 && *str < 32) || *str >= 127 ? _putchar('\\') + _putchar('x') + ((_puts(convert(*str, 16, 0, params) + 1)) ? 1 : _putchar('0')) + _puts(convert(*str, 16, 0, params)) : _putchar(*str);
-	return add;
+	return (add);
 }
 
 
