0012a21ae2bc2b117e7ebaeaafe3585abc199080
AsitaEmmanuella <asitemmanuella19@gmail.com>
2023-11-14 00:39:16 -0800

    p

--- printf_3_plus.c
+++ printf_3_plus.c
@@ -6,28 +6,48 @@
  * Return: number of bytes printed
  */
 
+
 int _printf(const char *format, ...)
 {
 	int add = 0;
 	va_list xy;
-	char *p;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
 	va_start(xy, format);
 
-	if (!format || (*format == '%' && !format[1]))
+	if (!format || (format[0] == '%' &&
+				(!format[1] || (format[1] == ' ' &&
+						!format[2]))))
 		return (-1);
 
 	for (p = (char *)format; *p; p++)
 	{
+		init_params(&params, xy);
+
 		if (*p != '%')
 		{
 			add += _putchar(*p);
 			continue;
 		}
 
-		p = get_and_process_format_spec(p, xy, &add);
-	}
+		start = ++p;
+		while (get_flag(p++, &params))
+			;
 
+		p = get_width(p, &params, xy);
+		p = get_precision(p, &params, xy);
+
+		if (get_modifier(p, &params))
+			p++;
+
+		if (!get_specifier(p))
+			add += print_from_to(start, p,
+					params.l_modifier || params.h_modifier ? p - 1 : 0)
+				;
+		else
+			add += get_print_func(p, xy, &params);
+	}
 	_putchar(BUF_FLUSH);
 	va_end(xy);
 	return (add);
